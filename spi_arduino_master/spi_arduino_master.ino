0db2e9c762404ff5c71ddd0726b486511f2e9077
Akshay P Kumar <93068695+akshayembedded@users.noreply.github.com>
2023-09-23 20:49:35 +0530

    Update spi_arduino_master.ino

--- spi_arduino_master/spi_arduino_master.ino
+++ spi_arduino_master/spi_arduino_master.ino
@@ -1,22 +1,16 @@
  #include <SPI.h>
 void setup() {
-     pinMode(9,OUTPUT);
-  digitalWrite(9,1);//
-    SPI.begin();
+     pinMode(9,OUTPUT);//D9 used for chip selection
+  digitalWrite(9,1);// to deselect device
+    SPI.begin();//MISO input MOSI output
    Serial.begin(9600);
   delay(1000);
-
-  
-  
 }
 
 void loop() {
-   SPI.beginTransaction(SPISettings(1000000, MSBFIRST, SPI_MODE3));
-  digitalWrite(9,0);
-  
+  SPI.beginTransaction(SPISettings(1000000, MSBFIRST, SPI_MODE3));//CPOL=1 CPHA=1
+  digitalWrite(9,0);//slave is selected
   Serial.print(SPI.transfer(0x03));
-
-    digitalWrite(9,1);
-  // put your main code here, to run repeatedly:
-
+  digitalWrite(9,1);
+ delay(1000);
 }
